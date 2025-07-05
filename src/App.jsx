884f60e6d11bcd6e4ebecc07583dc42cc5f839b8
ayathree <nobanitaayathree333@gmail.com>
2025-07-05 21:49:09 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <h1 className='text-8xl font-bold text-amber-600'>Essential</h1>
+      <p className='text-xl font-semibold text-amber'>Hi there</p>
+    </>
+  )
+}
+
+export default App
