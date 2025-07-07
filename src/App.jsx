6e6c8043fc086f99ef3b1cffcbd735e280ec91d6
ayathree <nobanitaayathree333@gmail.com>
2025-07-07 12:08:38 +0600

    editin the project name in vercel

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <>
       <h1 className='text-8xl font-bold text-amber-600'>Essential</h1>
-      <p className='text-xl font-semibold text-amber'>Hi there</p>
+      <p className='text-xl font-semibold text-amber'>Hi there this is cosmetic website</p>
     </>
   )
 }
