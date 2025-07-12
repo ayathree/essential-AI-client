18d287bd9dd389170660e6f4bd8d0fb82010bae9
ayathree <nobanitaayathree333@gmail.com>
2025-07-12 17:54:37 +0600

    setup sidebar in admin panel

--- src/admin/components/Sidebar.jsx
+++ src/admin/components/Sidebar.jsx
@@ -2,7 +2,13 @@
 
 const Sidebar = () => {
     return (
-        <div>
+        <div className="w-[18%] min-h-[100vh] border-r-[1px] py-[60px] fixed left-0 top-0">
+            <div className="flex flex-col gap-4 pt-[40px] pl-[20%] text-[15px]">
+                <div className="flex items-center justify-center md:justify-start gap-3 border border-grey-200 border-r-0 px-3 py-2 cursor-pointer hover:bg-[#663333]">Add Items</div>
+                <div className="flex items-center justify-center md:justify-start gap-3 border border-grey-200 border-r-0 px-3 py-2 cursor-pointer hover:bg-[#663333]">Add Items</div>
+                <div className="flex items-center justify-center md:justify-start gap-3 border border-grey-200 border-r-0 px-3 py-2 cursor-pointer hover:bg-[#663333]">Add Items</div>
+
+            </div>
             
         </div>
     );
--- src/admin/pages/AdminHome.jsx
+++ src/admin/pages/AdminHome.jsx
@@ -1,10 +1,12 @@
 import AdminNav from "../components/AdminNav";
+import Sidebar from "../components/Sidebar";
 
 
 const AdminHome = () => {
     return (
         <div className="w-[100vw] h-[100vh] relative">
             <AdminNav></AdminNav>
+            <Sidebar></Sidebar>
             
         </div>
     );
