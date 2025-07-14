38c6e92066266cedd7a6c463639b769822a642cb
ayathree <nobanitaayathree333@gmail.com>
2025-07-14 17:57:46 +0600

    editing list item page in admin panel

--- src/admin/pages/List.jsx
+++ src/admin/pages/List.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
+import AdminNav from "../components/AdminNav";
+import Sidebar from "../components/Sidebar";
 
 
 const List = () => {
+    const[list,setList]=useState([])
     return (
-        <div>
+        <div className="w-[100vw] min-h-[100vh] text-[#663333]">
+            <AdminNav></AdminNav>
+            <div className="w-[100%] h-[100%] flex items-center justify-start ">
+                <Sidebar></Sidebar>
+                <div className="w-[82%] h-[100%] lg:ml-[320px] md:ml-[230px] mt-[70px] flex flex-col gap-[30px] overflow-x-hidden py-[50px] ml-[100px]">
+                    <div className="w-[400px] h-[50px] text-[28px] md:text-[40px] mb-[20px] text-[#663333]">
+                        All Listed Products
+
+                    </div>
+                </div>
+            </div>
             
         </div>
     );
