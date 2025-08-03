38ebcc23f5e2d34e127d55345ab31bf057be239d
ayathree <nobanitaayathree333@gmail.com>
2025-08-03 22:20:08 +0600

    working on about us page

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -8,6 +8,7 @@ const About = () => {
             <div className="w-[100%] flex items-center justify-center flex-col lg:flex-row ">
                 <div className="lg:w-[50%] w-[100%] flex items-center justify-center">
                     <img src="https://i.ibb.co/237BmLss/image.png" alt="" className="lg:w-[65%] w-[80%] shadow-md shadow-black rounded-lg" />
+                    <p>the best products</p>
                 </div>
             </div>
             
