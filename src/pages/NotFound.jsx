d71313d4d75946a10fbb9012a108657a36baf9d2
ayathree <nobanitaayathree333@gmail.com>
2025-08-21 15:59:03 +0600

    complete the not found for admin

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,7 +6,7 @@ const NotFound = () => {
     return (
         <div className="w-[100vw] h-[100vh] md:text-[70px] text-[30px] flex items-center justify-center text-[#663333] flex-col gap-[20px]">
             404 Page Not Found
-            <button className="bg-[#663333] px-[20px] py-[10px] rounded-xl text-[18px] text-white cursor-pointer" onClick={()=>navigate('/login')}>Login</button>
+            <button className="bg-[#663333] px-[20px] py-[10px] rounded-xl text-[18px] text-white cursor-pointer" onClick={()=>navigate('/')}>Login</button>
             
         </div>
     );
